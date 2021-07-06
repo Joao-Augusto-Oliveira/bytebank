b35df39c06e56708b13907c408a30b717ab99745
Joao Augusto Oliveira dos Santos <joaoaugustofurg@gmail.com>
2021-07-05 21:37:21 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-rooti',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
